3aae23a420fb69e25f4f2f6cbeecb333610c575a
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-10-22 19:17:35 +0200

    Transfered code from laboratories
    Wrong filtriraj() logic, have to use realloc() in a for loop
    
    and fixing some memmory leaks

--- III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_02/main.c
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_02/main.c
@@ -2,103 +2,108 @@
 #include <stdlib.h>
 
 int *podniz(int *niz, int start, int stop) {
-  int *slice = (int *)malloc((stop - start + 1) * sizeof(int));
-  if (!slice)
-    return NULL;
+	int *slice = (int *)malloc((stop - start + 1) * sizeof(int));
+	if (!slice)
+		return NULL;
 
-  int j = 0;
-  for (int i = start; i <= stop; i++) {
-    slice[j++] = niz[i];
-  }
+	int j = 0;
+	for (int i = start; i <= stop; i++) {
+		slice[j++] = niz[i];
+	}
 
-  return slice;
+	return slice;
 }
 
 int *filtriraj(int *niz, int n, int th, int *nth) {
-  int *filtered = (int *)malloc(n * sizeof(int));
-  if (!filtered)
-    return NULL;
-  int j = 0;
-  for (int i = 0; i < n; i++) {
-    if (niz[i] < th) {
-      filtered[j++] = niz[i];
-    }
-  }
-  *nth = j;
-  return filtered;
+	int *filtered = NULL;
+	if (!filtered)
+		return NULL;
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (niz[i] < th) {
+			filtered = (int *)realloc(filtered, (j+1) * sizeof(int));
+			filtered[j++] = niz[i];
+		}
+	}
+	*nth = j;
+	return filtered;
 }
 
 int **podijeli(int *niz, int n) {
-  if (n % 2 != 0) {
-    printf("error: array can not be split equally\n");
-    return NULL;
-  }
-  int half_n = n / 2;
-  int rows = 2;
+	if (n % 2 != 0) {
+		printf("error: array can not be split equally\n");
+		return NULL;
+	}
+	int half_n = n / 2;
+	int rows = 2;
 
-  int **arr = (int **)malloc(rows * sizeof(int *));
-  if (!arr)
-    return NULL;
+	int **arr = (int **)malloc(rows * sizeof(int *));
+	if (!arr)
+		return NULL;
 
-  // alloc half_n lenght int arrays and copy elems from 'niz'
-  for (int i = 0, niz_counter = 0; i < rows; i++) {
-    arr[i] = (int *)malloc(half_n * sizeof(int));
-    if (!arr[i])
-      return NULL;
+	// alloc half_n lenght int arrays and copy elems from 'niz'
+	for (int i = 0, niz_counter = 0; i < rows; i++) {
+		arr[i] = (int *)malloc(half_n * sizeof(int));
+		if (!arr[i])
+			return NULL;
 
-    for (int j = 0; j < half_n; j++) {
-      arr[i][j] = niz[niz_counter++];
-    }
-  }
+		for (int j = 0; j < half_n; j++) {
+			arr[i][j] = niz[niz_counter++];
+		}
+	}
 
-  return arr;
+	return arr;
 }
 
 int main(void) {
-  // podniz()
-  printf("podniz():\n");
-  int arr_a[] = {1, 2, 3, 4, 5, 6, 7};
-  int *slice_a = podniz(arr_a, 1, 4);
-  if (!slice_a)
-    return 1;
+	// podniz()
+	printf("podniz():\n");
+	int arr_a[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int *slice_a = podniz(arr_a, 1, 4);
+	if (!slice_a)
+		return 1;
 
-  for (int i = 0; i < 4; i++)
-    printf("%d ", slice_a[i]);
-  printf("\n");
+	for (int i = 0; i < 4; i++)
+		printf("%d ", slice_a[i]);
+	printf("\n");
 
-  printf("\n");
+	printf("\n");
 
-  // filtriraj()
-  printf("filtriraj():\n");
-  int arr_b[] = {10, 30, 40, 20, 60, 70, 50};
-  int filtered_b_len;
-  int *filtered_b =
-      filtriraj(arr_b, (sizeof(arr_b) / sizeof(arr_b[0])), 40, &filtered_b_len);
-  if (!filtered_b)
-    return 2;
+	// filtriraj()
+	printf("filtriraj():\n");
+	int arr_b[] = { 10, 30, 40, 20, 60, 70, 50 };
 
-  for (int i = 0; i < filtered_b_len; i++)
-    printf("%d ", filtered_b[i]);
-  printf("\n");
+	int filtered_b_len;
+	int *filtered_b =
+		filtriraj(arr_b, (sizeof(arr_b) / sizeof(arr_b[0])), 40, &filtered_b_len);
+	if (!filtered_b)
+		return 2;
 
-  // podijeli
-  printf("\npodijeli():\n");
-  int arr_c[] = {0, 1, 2, 3, 10, 9, 8};
-  int c_len = 7;
-  int **split_c = podijeli(arr_c, c_len);
-  if (!split_c)
-    return 3;
+	for (int i = 0; i < filtered_b_len; i++)
+		printf("%d ", filtered_b[i]);
+	printf("\n");
 
-  for (int i = 0; i < 2; i++) {
-    printf("[ ");
-    for (int j = 0; j < c_len / 2; j++) {
-      printf("%d ", split_c[i][j]);
-    }
-    printf("]\n");
-  }
+	// podijeli
+	printf("\npodijeli():\n");
+	int arr_c[] = { 1, 2, 3, 10, 9, 8 };
+	int c_len = 6;
 
-  free(slice_a);
-  free(filtered_b);
-  free(split_c);
-  return 0;
+	int **split_c = podijeli(arr_c, c_len);
+	if (!split_c)
+		return 3;
+
+	for (int i = 0; i < 2; i++) {
+		printf("[ ");
+		for (int j = 0; j < c_len / 2; j++) {
+			printf("%d ", split_c[i][j]);
+		}
+		printf("]\n");
+	}
+
+	free(slice_a);
+	free(filtered_b);
+	for (int i = 0; i < 2; i++)
+		free(split_c[i]);
+	free(split_c);
+	return 0;
 }
---
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_02/main2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	float x, y;
+}Tocka;
+
+typedef struct
+{
+	Tocka* arr;
+	int n;
+}Poligon;
+
+Poligon* novi_poligon(float *niz_x, float *niz_y, int n) {
+	if (n < 3) {
+		printf("\nerror: invalid array of points len\n");
+		return NULL;
+	}
+
+	Poligon *poly = (Poligon*)malloc(sizeof(Poligon));
+	if (!poly)
+		return NULL;
+
+	poly->arr = (Tocka*)malloc(n * sizeof(Tocka));
+	if (!poly->arr)
+		return NULL;
+	poly->n = n;
+
+	for (int i = 0; i < poly->n; i++)
+	{
+		poly->arr[i].x = niz_x[i];
+		poly->arr[i].y = niz_y[i];
+	}
+
+	return poly;
+}
+
+Tocka** pozitivni(Poligon *p, int *np) {
+	Tocka** arr = (Tocka**)malloc(p->n * sizeof(Tocka*));
+	if (!arr)
+		return NULL;
+	int j = 0;
+	for (int i = 0; i < p->n; i++)
+	{
+		if (p->arr[i].x > 0 && p->arr[i].y > 0) {
+			arr[j++] = &(p->arr[i]);
+		}
+	}
+	*np = j;
+	return arr;
+}
+
+int main() {
+	float x[] = { 1.1, -2.111111, 3.23 };
+	float y[] = { 4.237, -5.093, 6 };
+	int n = 3;
+
+	printf("poligon:\n");
+	Poligon* poly = novi_poligon(x, y, n);
+	if (!poly)
+		return 1;
+
+	for (int i = 0; i < poly->n; i++)
+		printf("%.2f, %.2f | ", poly->arr[i].x, poly->arr[i].y);
+	printf("\n");
+
+	// tocka double pointer
+	printf("\ntocke:\n");
+	int np;
+	Tocka** arr = pozitivni(poly, &np);
+	if (!arr)
+		return 2;
+
+	for (int i = 0; i < np; i++)
+	{
+		printf("%.2f, %.2f | ", arr[i]->x, arr[i]->y);
+	}
+	printf("\n");
+
+	free(poly->arr);
+	free(poly);
+	free(arr);
+}
