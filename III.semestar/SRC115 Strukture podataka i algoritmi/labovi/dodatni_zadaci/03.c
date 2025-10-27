095d8a1e2c42690527dd991c47c252e370d31fe0
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-10-27 12:29:20 +0100

    04 done

--- III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/03.c
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/03.c
@@ -26,7 +26,7 @@ int main(void) {
   }
 
   FILE *file;
-  file = fopen("01.bin", "wb");
+  file = fopen("04.bin", "wb");
   if (!file) {
     perror("");
     return 1;
---
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/04.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+4) Napisati program koji od korisnika cita jedan broj odnosno indeks.
+Program iz datoteke iz prethodnog zadatka cita i ispisuje na ekran
+koordinatu koja odgovara tom indeksu. (koristite fseek)
+*/
+
+typedef struct {
+  float x, y;
+} Coordinate;
+
+int main(void) {
+  FILE *file;
+  file = fopen("04.bin", "rb");
+  if (!file) {
+    perror("");
+    return 1;
+  }
+
+  int index;
+  printf("unesi index: ");
+  scanf(" %d", &index);
+
+  Coordinate indexed_coordinate;
+  if (fseek(file, sizeof(Coordinate) * index + sizeof(int), SEEK_SET))
+    return 2;
+
+  int check;
+  check = fread(&indexed_coordinate.x, sizeof(indexed_coordinate.x), 1, file);
+  if (check < 1) {
+    puts("Error reading coordinate.x from file. (might be an overflow, check "
+         "the index)");
+    return 3;
+  }
+  check = fread(&indexed_coordinate.y, sizeof(indexed_coordinate.x), 1, file);
+  if (check < 1) {
+    puts("Error reading coordinate.y from file. (might be an overflow, check "
+         "the index)");
+    return 4;
+  }
+
+  fclose(file);
+  // print
+
+  printf("\n[%d]: (%f, %f)\n", index, indexed_coordinate.x,
+         indexed_coordinate.y);
+
+  return 0;
+}
