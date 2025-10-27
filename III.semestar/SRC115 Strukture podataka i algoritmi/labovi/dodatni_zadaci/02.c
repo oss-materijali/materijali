4689663503f11d31372ef2a4ce9aacce12016da3
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-10-27 12:07:55 +0100

    fixed 02 errors and freeing, 03 finished

--- III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/02.c
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/02.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 
 /*
-2) Napisati program koji čita datoteku iz prethodnog zadatka (sa
+2) Napisati program koji cita datoteku iz prethodnog zadatka (sa
 fread) i ispisuje brojeve na ekran.
 */
 
 int main(void) {
   FILE *file;
   file = fopen("01.bin", "rb");
+  if (!file) {
+    perror("");
+    return 1;
+  }
+
   int n;
   int check = fread(&n, sizeof(int), 1, file);
   if (check < 1) {
     puts("Error reading n from file.");
+    return 2;
   }
 
   int *arr = (int *)malloc(n * sizeof(int));
@@ -20,16 +26,19 @@ int main(void) {
   check = fread(arr, sizeof(arr[0]), n, file);
   if (check < 1) {
     puts("Error reading arr from file.");
+    return 3;
   }
 
-  // print
-  printf("n: %d\n", n);
+  fclose(file);
 
+  // output
+  printf("n: %d\n", n);
   printf("arr: ");
   for (int i = 0; i < n; i++) {
     printf("%d ", arr[i]);
   }
   printf("\n");
 
+  free(arr);
   return 0;
 }
---
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/dodatni_zadaci/03.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+3) Napisati program koji od korisnika cita n struktura u niz i zatim ih
+sprema u binarnu datoteku. Prije samog niza se sprema broj n. Broj
+n se isto cita od korisnika prije citanja struktura. Strukture su tipa
+Koordinata i sastoje se od dva float broja.
+*/
+
+// task 1) but with structs (more complex data type, same shi really)
+
+typedef struct {
+  float x, y;
+} Coordinate;
+
+int main(void) {
+  int n;
+  printf("unesi n: ");
+  scanf(" %d", &n);
+
+  Coordinate arr[n];
+  printf("\nunesi arr struktura: \n");
+  for (int i = 0; i < n; i++) {
+    printf("\t[%d]: ", i);
+    scanf(" %f %f", &arr[i].x, &arr[i].y);
+  }
+
+  FILE *file;
+  file = fopen("01.bin", "wb");
+  if (!file) {
+    perror("");
+    return 1;
+  }
+
+  int check = fwrite(&n, sizeof(n), 1, file);
+  if (check < 1) {
+    puts("Error writing the n in the file");
+    return 2;
+  }
+  check = fwrite(arr, sizeof(arr[0]), n, file);
+  if (check < n) {
+    puts("Error writing the array to file");
+    return 3;
+  }
+
+  // open file in a hex editor to see output
+
+  fclose(file);
+  return 0;
+}
