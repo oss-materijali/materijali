e0ddd009e154ed95da42a1ff2e55e3686281b94d
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-10-27 15:28:54 +0100

    starting SPA 3rd lab exercise
    holy shit

---
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_03/include/stl.h
@@ -0,0 +1,35 @@
+#ifndef STL
+#define STL
+
+#include <stdio.h>
+
+typedef struct {
+  float x, y, z; // 4 bytes * 3 = 12
+} Vertex;
+
+#pragma pack(push, 1)
+typedef struct {
+  Vertex normal;        // 12
+  Vertex vertices[3];   // 12 * 3 = 36
+  unsigned short color; // 2
+} Triangle;
+// 12 + 36 + 2 = 50
+// cpu: 4-byte alloc, 4*13=52, need 50 bytes EXACTLY
+// pragma to force cpu to 1-byte alloc for this struct only
+#pragma pack(pop)
+
+typedef struct {
+  Triangle *arr;
+  int n;
+} Object3D;
+
+// Funkciju koja čita binarnu STL datoteku i vraća ispunjenu Objekt3D strukturu
+Object3D *read_stl_bin(FILE *file);
+// Funkciju koja Objekt3D strukturu zapisuje u binarnu STL datoteku
+FILE *write_stl_bin(Object3D object);
+// Funkcija koja Objekt3D strukturu zapisuje u tekstualnu STL datoteku
+FILE *write_stl_text(Object3D object);
+// Funkciju koja briše Objekt3D strukturu
+void free_object3d(Object3D object);
+
+#endif
---
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_03/src/main.c
@@ -0,0 +1,15 @@
+#include "../include/stl.h"
+#include <stdio.h>
+
+/*
+Translation:
+  Trokut => Triangle
+  Vrh => Vertex
+  Tocke => Vertices
+  Objekt => Object
+*/
+
+int main(void) {
+  //
+  return 0;
+}
