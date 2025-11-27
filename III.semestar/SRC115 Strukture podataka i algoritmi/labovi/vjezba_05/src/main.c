f43e844416854be039831b2c663c0ef9f7adb5c7
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-11-27 12:18:43 +0100

    i forgot half of the task lol.., now its finished

--- III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_05/src/main.c
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_05/src/main.c
@@ -1,6 +1,8 @@
 #include "dictionary.h"
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int
 readWord(FILE* fd, char* buffer)
@@ -26,6 +28,40 @@ readWord(FILE* fd, char* buffer)
 }
 
 int
+filter(Word* w)
+{
+  if (!w || !w->word)
+    return 0;
+
+  return (w->count > 5 && w->count < 10 && strlen(w->word) > 3);
+}
+
+void
+filterDictionary(Dictionary indict, int (*filter)(Word* w))
+{
+  if (!indict || !filter)
+    return;
+
+  Word* prev = indict;
+  Word* cur = indict->next;
+  if (cur == NULL) // list empty
+    return;
+
+  while (cur != NULL) {
+    if (!filter(cur)) {
+      Word* del = cur;
+      cur = cur->next;
+      prev->next = cur;
+      free(del->word);
+      free(del);
+    } else {
+      prev = cur;
+      cur = cur->next;
+    }
+  }
+}
+
+int
 main()
 {
   FILE* fd;
@@ -40,12 +76,16 @@ main()
 
   dict = create();
   while (readWord(fd, buffer)) {
-    printf("%s\n", buffer);
+    // printf("%s\n", buffer);
     add(dict, buffer);
   }
 
   fclose(fd);
 
+  print(dict);
+
+  printf("filtered:\n");
+  filterDictionary(dict, filter);
   print(dict);
 
   destroy(dict);
