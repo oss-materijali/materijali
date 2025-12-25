d634b04f9b7263b6814aece9ff13bc734071110d
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-12-25 14:53:04 +0100

    basic logic done

--- III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_08/Hash/hash.c
+++ III.semestar/SRC115 Strukture podataka i algoritmi/labovi/vjezba_08/Hash/hash.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "string.h"
+
 #define HCONST 3567892
 
 HashTable *NewTable(int size)
@@ -12,15 +14,15 @@ HashTable *NewTable(int size)
 	HashTable *new = malloc(sizeof(HashTable));
 
 	new->table = malloc(sizeof(Bin) * size);
-	for (int i = 0; i < size; i++)
-	{
-		new->table[i]->next = NULL;
-		new->table[i]->word = NULL;
-	}
 	new->size = size;
 	new->load = 0;
 
-	return NULL;
+	for (int i = 0; i < new->size; i++)
+	{
+		new->table[i] = NULL;
+	}
+
+	return new;
 }
 
 unsigned int hash(char *word)
@@ -38,37 +40,75 @@ unsigned int hash(char *word)
 void Insert(HashTable *ht, char *word)
 {
 	// dodaje novu rijec u listu na odgovarajucem pretincu
+
+	// Adds a new node at the start of the list O(1)
+	// Appending to the list is O(N) for collissions
+
 	int index = hash(word) % ht->size;
-	Bin *bin_ptr = ht->table[index];
-	if (bin_ptr->word == NULL)
-	{
-		bin_ptr->word = word;
-	}
-	else
-	{
-		while (bin_ptr->next != NULL)
-		{
-			if (strcmp(bin_ptr->word, word) == 0)
-				return;
 
-			bin_ptr = bin_ptr->next;
-		}
-		Bin *new = malloc(sizeof(Bin));
-		new->word = NULL;
-		new->next = NULL;
+	// table[i] -> old -> NULL
+	Bin *new = malloc(sizeof(Bin));
+	new->word = strdup(word);
+	new->next = ht->table[index];
+	ht->table[index] = new;
 
-		bin_ptr->word = word;
-		bin_ptr->next = new;
-	}
+	ht->load++;
+	// table[i] -> new -> old -> NULL
 }
 
 int Get(HashTable *ht, char *word)
 {
 	// vraca 0 ili 1 ovisno o tome da li rijec postoji u tablici
+	int index = hash(word) % ht->size;
+	Bin *cur = ht->table[index];
+	while (cur != NULL)
+	{
+		if (strcmp(cur->word, word) == 0)
+			return 1;
+
+		cur = cur->next;
+	}
+
 	return 0;
 }
 
 void DeleteTable(HashTable *ht)
 {
 	// brise cijelu hash tablicu (liste na svim pretincima (rijec i element liste), pretince ...)
+	if (!ht)
+		return;
+
+	for (int i = 0; i < ht->size; i++)
+	{
+		Bin *cur = ht->table[i];
+		while (cur != NULL)
+		{
+			Bin *del = cur;
+			cur = cur->next;
+
+			free(del->word);
+			free(del);
+		}
+	}
+
+	free(ht->table);
+	free(ht);
 }
+
+/*
+// brise cijeli rjecnik
+void
+destroy(Dictionary dict)
+{
+  if (!dict)
+ return;
+  Word* cur = dict->next;
+  while (cur != NULL) {
+ Word* del = cur;
+ cur = cur->next;
+ free(del->word);
+ free(del);
+  }
+  free(dict);
+}
+*/
